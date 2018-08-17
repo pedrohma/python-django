cec67e0d9497b10a98bf54640d7f7a49cf846130
Pedro Henrique de Miranda Arthur <pedrohma95@gmail.com>
2018-08-17 16:00:21 -0700

    finishing

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -12,7 +12,13 @@ class Question(models.Model):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
     def was_published_recently(self):
         now = timezone.now()
-        return now - datetime.timedelta(days=1) <= self.pub_date <= now    
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+    # was_published_recently.admin_order_field = 'pub_date'
+    # was_published_recently.boolean = True
+    # was_published_recently.short_description = 'Published recently?'   
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
